67c08316280937015ba96fe03910733fe195b21b
Dan <dan@peeron.com>
2016-09-19 15:25:21 -0700

    Improve logging, use correct save name

--- run_headless.sh
+++ run_headless.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -ex
 
+function log {
+  echo "$*"
+  /usr/bin/logger -t run-headless "$*"
+}
+
 # Grab configuration from the environment, or use defaults
 SAVENAME="${FACTORIO_SAVENAME:-headless}"
 LATENCY="${FACTORIO_LATENCY_MS:-50}"
@@ -13,31 +18,33 @@ SAVEDIR="/opt/factorio/saves"
 SAVEPATH="$SAVEDIR/$SAVENAME.zip"
 
 if [ ! -f "$SAVEPATH" ]; then
-  /opt/factorio/bin/x64/factorio --create "$SAVENAME"
+  log creating a new save: $SAVEPATH
+  /opt/factorio/bin/x64/factorio --create "$SAVEDIR/$SAVENAME"
 else
   # Check if there's a more recent autosave (e.g. server crashed)
   LATEST_SAVEFILE="$(ls -t "$SAVEDIR" 2>/dev/null | head -1)"
   if [ -z "$LATEST_SAVEFILE" ]; then
-    echo "Couldn't identify latest save game in $SAVEDIR, aborting."
+    log "Couldn't identify latest save game in $SAVEDIR, aborting."
     exit 1
   fi
 
   # If the more recent save isn't the main save, replace the main save
   if [ "$LATEST_SAVEFILE" == "$SAVENAME.zip" ]; then
-    echo "Save is up to date - using $SAVEPATH."
+    log "Save is up to date - using $SAVEPATH."
   else
     if echo "$LATEST_SAVEFILE" | grep -qF "_autosave"; then
-      echo "Found more recent save $LATEST_SAVEFILE (server crashed?) - transferring to $SAVEPATH."
+      log "Found more recent save $LATEST_SAVEFILE (server crashed?) - transferring to $SAVEPATH."
       cp "$SAVEDIR/$LATEST_SAVEFILE" "$SAVEPATH"
     else
-      echo "Found a more recent save $LATEST_SAVEFILE, but it doesn't appear to be an autosave. Ignoring."
+      log "Found a more recent save $LATEST_SAVEFILE, but it doesn't appear to be an autosave. Ignoring."
     fi
   fi
 fi
 
 # Run the server
+log starting server...
 /opt/factorio/bin/x64/factorio \
-  --start-server "$SAVENAME" \
+  --start-server "$SAVEPATH" \
   --latency-ms "$LATENCY" \
   --autosave-interval "$AUTOSAVE_INTERVAL" \
   --autosave-slots "$AUTOSAVE_SLOTS" \
